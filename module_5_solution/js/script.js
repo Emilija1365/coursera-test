30ee85c8f674ca957abe8430e69b124e9e703793
Emilija <eandjieva@darts-ip.com>
2018-08-05 22:30:55 +0200

    mod5

--- module_5_solution/js/script.js
+++ module_5_solution/js/script.js
@@ -115,11 +115,6 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 // $dc.loadMenuItems('L')
                 // Hint: you need to surround the chosen category short name with something before inserting
                 // it into the home html snippet.
-                //
-                // var homeHtmlToInsertIntoMainPage = chosenCategoryShortName;
-                // var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
-                //     "catShortName",
-                //     "'" +chosenCategoryShortName.short_name + "'");
 
                 var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
                     "randomCategoryShortName",
